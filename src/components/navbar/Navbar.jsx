796372591cebfd17a8d97bae841b1505da9b10cc
Tarek994 <ghali.tarek.123@hotmail.com>
2023-05-29 23:26:18 +0300

    feat(navbar): add the navegation buttons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,21 @@ const Navbar = () => {
     <div className="main-nav">
       <div className="nav">
         <div className="right"></div>
-       
+        <div className="left">
+
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+
+        </div>
       </div>
     </div>
   );
