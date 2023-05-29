74185a3307be4cfba8b49343ad8fb88e7450d428
Tarek994 <ghali.tarek.123@hotmail.com>
2023-05-29 23:28:56 +0300

    chore(root): formatting the code

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,19 +7,17 @@ const Navbar = () => {
       <div className="nav">
         <div className="right"></div>
         <div className="left">
-
-            <ul>
-              <li>
-                <a href="/">Home</a>
-              </li>
-              <li>
-                <a href="/about">About</a>
-              </li>
-              <li>
-                <a href="/contact">Contact</a>
-              </li>
-            </ul>
-
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
