8473f5dd4aac7a6126afc1568de3c81234caf94d
Tarek994 <ghali.tarek.123@hotmail.com>
2023-05-29 23:38:21 +0300

    chore(app.js): import the navbar to the App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import "../src/components/navbar/Navbar.jsx"
+import Navbar from '../src/components/navbar/Navbar.jsx';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
     </div>
   );
 }
