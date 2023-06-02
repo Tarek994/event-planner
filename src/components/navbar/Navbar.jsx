2d6c98e0b3a6db07923784a3229d4ae83e61037b
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-02 23:57:19 +0300

    feat(navbar): add the state for the menu-bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="main-nav">
       <div className="bar">
