edb73e60b4cf92f9059301e5d4bb8616b80c062d
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-04 05:19:17 +0300

    docs(Images.jsx): add the images component to export the images

---
+++ src/components/home/slider/images.jsx
@@ -0,0 +1,9 @@
+import white from "../../../assets/white.jpg";
+import white1 from "../../../assets/white1.jpg";
+import white2 from "../../../assets/whiteParty.webp";
+
+export const images = [
+    { src: white, alt: "1", caption: "Caption 1" },
+    { src: white1, alt: "2", caption: "Caption 2" },
+    { src: white2, alt: "3", caption: "Caption 3" },
+  ];
