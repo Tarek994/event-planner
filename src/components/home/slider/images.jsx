43d257dee4139be334d55123bbcc2e9a5ed9a142
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-05 23:43:53 +0300

    feat(Images.jsx): change the caption of the slider images

--- src/components/home/slider/images.jsx
+++ src/components/home/slider/images.jsx
@@ -3,7 +3,7 @@ import white1 from "../../../assets/white1.jpg";
 import white2 from "../../../assets/whiteParty.webp";
 
 export const images = [
-    { src: white, alt: "1", caption: "Caption 1" },
-    { src: white1, alt: "2", caption: "Caption 2" },
-    { src: white2, alt: "3", caption: "Caption 3" },
+    { src: white, alt: "1", caption: "Weddings" },
+    { src: white1, alt: "2", caption: "Equipment" },
+    { src: white2, alt: "3", caption: "Innovative designs" },
   ];
