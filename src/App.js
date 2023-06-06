9938ad903475b7a0d5140ba0c152d5703a20cca4
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-06 23:47:01 +0300

    chore(App.js): import the about component to the app js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import "../src/components/navbar/Navbar.jsx"
 import Navbar from '../src/components/navbar/Navbar.jsx';
 import Home from './components/home/Home';
+import About from './components/about/About.jsx';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
+      <About/>
     </div>
   );
 }
