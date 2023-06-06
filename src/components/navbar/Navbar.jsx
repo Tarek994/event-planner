19f015e6f527575ef1d4c769a24f0a581399d1b6
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-06 23:19:07 +0300

    feat(navbar.jsx): add the about button to the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -26,6 +26,9 @@ const Navbar = () => {
               <a href="/about">About</a>
             </li>
             <li>
+              <a href="/gallery">Gallery</a>
+            </li>
+            <li>
               <a href="/contact">Contact</a>
             </li>
           </ul>
