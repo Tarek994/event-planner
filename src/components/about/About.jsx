7e9ffe0ecca0e1e847da04b7721cd09055f4b5b5
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-06 23:34:24 +0300

    feat(about): add the about partitions

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
 
-const about = () => {
+
+const About = () => {
   return (
-    <div>about</div>
+    <div className="gallery">
+      <div className="left col-8">
+        <img src="" alt="" />
+      </div>
+      <div className="right col-4">
+        <h3 className="title"> Who We Are </h3>
+      </div>
+    </div>
   )
 }
-
-export default about
+export default About
