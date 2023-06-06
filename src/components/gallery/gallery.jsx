79ab920b904b062b38b7d720d0070a16b176b23c
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-06 23:27:03 +0300

    feat(gallery): create the new gallery component

---
+++ src/components/gallery/gallery.jsx
@@ -0,0 +1,11 @@
+ 
+const gallery = () => {
+  return (
+    <div className='gallery'>
+      <div className="left col-8"></div>
+      <div className="right col-4"></div>
+    </div>
+  )
+}
+
+export default gallery
