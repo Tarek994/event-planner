b0ff526d6483686039c7c769ecb069211dac09a1
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-07 23:56:32 +0300

    feat(About.jsx): import the image to the about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,15 +1,17 @@
-
+import "./about.css";
+import who from "../../assets/whoWeAre.jpg";
 
 const About = () => {
   return (
-    <div className="gallery">
-      <div className="left col-8">
-        <img src="" alt="" />
+    <div id="about" className="about row col-md-12">
+      <div className="col-8 left">
+        <img className="img-fluid" src={who} alt="" />
       </div>
-      <div className="right col-4">
+      <div className="right col-md-4">
         <h3 className="title"> Who We Are </h3>
       </div>
     </div>
-  )
-}
-export default About
+  );
+};
+
+export default About;
