aea6a351dd33240f9acb661bd6c937c3a74420ba
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-09 23:49:29 +0300

    chore(app.js): import the gallery component to the app.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "../src/components/navbar/Navbar.jsx"
 import Navbar from '../src/components/navbar/Navbar.jsx';
 import Home from './components/home/Home';
 import About from './components/about/About.jsx';
-
+import Gallery from './components/gallery/gallery';
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
       <About/>
+      <Gallery/>
     </div>
   );
 }
