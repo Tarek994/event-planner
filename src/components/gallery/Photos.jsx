6fe2f2d91b895bb792127935a9cc026ead3de9c4
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-10 23:38:55 +0300

    feat(gallery): add photo data

---
+++ src/components/gallery/Photos.jsx
@@ -0,0 +1,18 @@
+import decore from "../../assets/decor.jpg"
+import download from "../../assets/download (1).jpeg"
+import download1 from "../../assets/download.jpeg"
+import images from "../../assets/images.jpeg"
+import photo from "../../assets/photo.jpg"
+import qatar from "../../assets/qatar.jpg"
+
+export const Photos = [
+    { src: decore, alt: "1", caption: "Weddings" },
+    { src: download, alt: "2", caption: "Equipment" },
+    { src: download1, alt: "3", caption: "Innovative designs" },
+    { src: images, alt: "4", caption: "Weddings" },
+    { src: photo, alt: "5", caption: "Equipment" },
+    { src: qatar, alt: "6", caption: "Innovative designs" },
+  ];
+
+
+
