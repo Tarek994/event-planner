e9a51319654f0032fb4d5f3ca18b6559a895e074
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-19 23:55:33 +0300

    feat(Contact.jsx): add the title to the contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,10 @@
 const Contact = () => {
   return (
     <div id='contact' className='contact'>
-      
+      <div className="head">
+        <h3>Hello let's be in touch </h3>
+        
+      </div>
     </div>
   )
 }
