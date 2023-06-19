2498c5f72836180e1addbc725c52d341c91866ba
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-19 23:58:28 +0300

    chore(Contact.jsx): formatting the code

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,9 +6,9 @@ const Contact = () => {
       <div className="head">
         <h3>Hello let's be in touch </h3>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-             Optio autem praesentium eos harum ipsum accusantium?
-              Autem maiores soluta alias, dolore nostrum blanditiis,
-               provident incidunt officia quo possimus, dolorem expedita id!</p>
+            Optio autem praesentium eos harum ipsum accusantium?
+            Autem maiores soluta alias, dolore nostrum blanditiis,
+            provident incidunt officia quo possimus, dolorem expedita id!</p>
       </div>
     </div>
   )
