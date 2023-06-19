357e87ae34d1c90b7a0753e6ba1a348deb29b3b4
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-19 23:49:10 +0300

    chore(App.js): comment out unused component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar.jsx";
 import Home from "./components/home/Home.jsx";
 import About from "./components/about/About.jsx";
 import Gallery from "./components/gallery/gallery.jsx";
+import Contact  from "./components/contact/Contact.jsx";
 
 const App = () => {
   return (
@@ -11,9 +12,9 @@ const App = () => {
       <Navbar />
       <div id="section-1"></div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/gallery" element={<Gallery />} /> */}
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
