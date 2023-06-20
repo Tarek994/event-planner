1895c160c53c3768bfe2e7b9dfca526bc0bce5b2
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-20 15:05:16 +0300

    add the className for the head and title of the contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -21,8 +21,8 @@ const Contact = () => {
   return (
     <div id='contact' className='contact'>
       <div className="head">
-        <h3>Hello let's be in touch </h3>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        <h3 className="con-title">Hello let's be in touch </h3>
+        <p className="con-bio">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
             Optio autem praesentium eos harum ipsum accusantium?
             Autem maiores soluta alias, dolore nostrum blanditiis,
             provident incidunt officia quo possimus, dolorem expedita id!</p>
